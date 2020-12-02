12df49a2cb9baf226204af554f7980557a150428
Erik Behrends <behrends@users.noreply.github.com>
2020-12-02 11:54:15 +0100

    Übung: Random als Klasse mit state und Zufallszahl

--- components/Random.js
+++ components/Random.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function Random({ label, value }) {
-  return (
-    <View>
-      <Text style={styles.counter}>{value}</Text>
-      <Button title={label} onPress={() => alert('Es klappt 1')} />
-    </View>
-  );
+export default class Random extends React.Component {
+  state = { value: this.props.value };
+
+  render() {
+    const { label } = this.props;
+    const { value } = this.state;
+    return (
+      <View>
+        <Text style={styles.counter}>{value}</Text>
+        <Button
+          title={label}
+          onPress={() =>
+            this.setState({ value: Math.floor(Math.random() * 1000) })
+          }
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
