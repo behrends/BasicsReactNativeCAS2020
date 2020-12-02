3d0395020a9c7de108c8d6fe479fc259290c39bb
Erik Behrends <behrends@users.noreply.github.com>
2020-12-02 11:19:59 +0100

    Komponente Random (Übung)

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Counter from './components/Counter';
+import Random from './components/Random';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Counter value="0" />
-      <Counter value="10" />
-      <Counter value="100" />
+      <Counter label="Zähler1" value="0" />
+      <Counter label="Zähler2" value="10" />
+      <Counter label="Zähler3" value="100" />
+      <Random label="Zufall" value="123" />
     </View>
   );
 }
--- components/Random.js
+++ components/Random.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function Counter(props) {
+export default function Random(props) {
   const value = props.value;
   const label = props.label;
   return (
