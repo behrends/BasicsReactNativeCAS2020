b666f3ee7d45203c5e5febf3482288c0ab8e4b41
Erik Behrends <behrends@users.noreply.github.com>
2020-12-02 11:31:27 +0100

    Counter als Klasse

--- components/Counter.js
+++ components/Counter.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function Counter({ label, value }) {
-  return (
-    <View>
-      <Text style={styles.counter}>{value}</Text>
-      <Button title={label} onPress={() => alert('Es klappt 1')} />
-    </View>
-  );
+// export default function Counter({ label, value }) {
+//   return (
+//     <View>
+//       <Text style={styles.counter}>{value}</Text>
+//       <Button title={label} onPress={() => alert('Es klappt 1')} />
+//     </View>
+//   );
+// }
+
+export default class Counter extends React.Component {
+  render() {
+    const { label, value } = this.props;
+    return (
+      <View>
+        <Text style={styles.counter}>{value}</Text>
+        <Button title={label} onPress={() => alert('Es klappt 1')} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
