74af3c3739cdcef04ab4ee1bf503cc591004a796
Erik Behrends <behrends@users.noreply.github.com>
2020-12-02 11:41:40 +0100

    Counter als Klasse mit state

--- App.js
+++ App.js
@@ -6,10 +6,10 @@ import Random from './components/Random';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Counter label="Zähler1" value="0" />
-      <Counter label="Zähler2" value="10" />
-      <Counter label="Zähler3" value="100" />
-      <Random label="Zufall" value="123" />
+      <Counter label="Zähler1" value={0} />
+      <Counter label="Zähler2" value={10} />
+      <Counter label="Zähler3" value={100} />
+      <Random label="Zufall" value={123} />
     </View>
   );
 }
--- components/Counter.js
+++ components/Counter.js
@@ -11,12 +11,23 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 // }
 
 export default class Counter extends React.Component {
+  // initialen Zustand (state) beim ersten Anzeigen der Komponente
+  state = { value: this.props.value };
+
+  // render wird von React automatisch ausgeführt, wenn
+  // a) die Komponente im UI erscheint (mit initialem state)
+  // b) der Zustand in state sich ändert (Aufruf von this.setState)
+  // c) die props einer Komponente sich ändern (z.B. bei Text unten)
   render() {
-    const { label, value } = this.props;
+    const { label } = this.props;
+    const { value } = this.state;
     return (
       <View>
         <Text style={styles.counter}>{value}</Text>
-        <Button title={label} onPress={() => alert('Es klappt 1')} />
+        <Button
+          title={label}
+          onPress={() => this.setState({ value: value + 1 })}
+        />
       </View>
     );
   }
