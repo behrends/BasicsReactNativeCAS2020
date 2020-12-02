b1be1f943809fd45a4ae1b3921be6c720e2f6738
Erik Behrends <behrends@users.noreply.github.com>
2020-12-02 10:44:25 +0100

    Button hinzugefügt

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.counter}>123</Text>
+      <Button
+        title="Button1"
+        onPress={() => alert('Es klappt!!!!')}
+      />
     </View>
   );
 }
