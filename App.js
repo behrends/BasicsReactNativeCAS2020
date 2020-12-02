0f317da6a36ff3ac8c6d0005957872debc8cdf2e
Erik Behrends <behrends@users.noreply.github.com>
2020-12-02 10:38:43 +0100

    Text als Zähler gestyled

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.counter}>123</Text>
     </View>
   );
 }
@@ -15,5 +15,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  counter: {
+    color: 'orange',
+    fontSize: 82,
   },
 });
